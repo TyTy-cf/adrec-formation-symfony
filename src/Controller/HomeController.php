aff298659f126921627e35d817364923b6ab440c
Wendy <huleuwendy@gmail.com>
2021-01-07 14:02:44 +0100

    modif exo1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -26,25 +26,22 @@ class HomeController extends AbstractController
                 'b2' => 'Orangina',
                 'b3' => 'Vin rouge',
                 'b4' =>  'Sprite',
-                'b5' => 'Eau',
-                'b6' =>  'Badoit',
-                'b7' =>  'Rosé',
-                'b8' =>  'Vodka',
+                'Eau', 'Badoit', 'Rosé', 'Vodka'
             ],
             'date' => new \DateTime(),
         ]);
     }
 
-    /**
-     * @Route(path="/index", name="boissons_index")
-     * @return Response
-     */
-    public function boissonsIndex(): Response
-    {
-        return $this->render('boissons.html.twig', [
-            'boissons' => ['Eau', 'Badoit', 'Rosé', 'Vodka'],
-        ]);
-    }
+//    /**
+//     * @Route(path="/index", name="boissons_index")
+//     * @return Response
+//     */
+//    public function boissonsIndex(): Response
+//    {
+//        return $this->render('boissons.html.twig', [
+//            'boissons' => ['Eau', 'Badoit', 'Rosé', 'Vodka'],
+//        ]);
+//    }
 
     /**
      * @Route(path="/boisson/show/{boisson}", name="boisson_show")
